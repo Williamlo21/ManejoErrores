e71bb86f80ed7a2935a0795299a3e251a70d7543
William lopez <lopezrebellonw@gmail.com>
2023-11-28 19:23:27 -0500

    se empieza a implementar controladores por defecto y enrutamientos

---
+++ ejercicio2/task/config/parameters.php
@@ -0,0 +1,4 @@
+<?php
+define("base_url", "http://localhost/adso/villamil/manejoErrores/ejercicio2/task/");
+define("controller_default","ProductoController");
+define("action_default","index");
---
+++ ejercicio2/task/view/layouts/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <ul>
+        <li><a href="<?= base_url ?>estudiantes/registrar">Obtener promedio</a></li>
+        <li><a href="<?= base_url ?>respuestas/registrar">Resultado de Pruebas</a></li>
+        <li><a href="<?= base_url ?>partidos/registrar">Resultado de partidos</a></li>
+    </ul>
+    <h1>Aplicacion para tener promedios, mostrar resultados de pruebas y resultados de partidos</h1>
